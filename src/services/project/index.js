f12ab7df8d0a3d462947079ac6f09f4d918176ff
rafipopz98 <itsrealrafi@gmail.com>
2023-10-06 19:28:58 +0530

    vercel push and animation

--- src/services/project/index.js
+++ src/services/project/index.js
@@ -24,7 +24,7 @@ export const addProject = async (formData) => {
 export const getAllAdminProjects = async () => {
   try {
     const responses = await fetch(
-      "https://landmarkconstructions.vercel.app/api/admin/allProjects",
+      "https://landmarkconstructions.netlify.app/api/admin/allProjects",
       {
         method: "GET",
         cache: "no-store",
@@ -42,7 +42,7 @@ export const getAllAdminProjects = async () => {
 export const projectBYYId = async (id) => {
   try {
     const res = await fetch(
-      `https://landmarkconstructions.vercel.app/admin/projectById?id=${id}`,
+      `https://landmarkconstructions.netlify.app/admin/projectById?id=${id}`,
       {
         method: "GET",
         cache: "no-store",
