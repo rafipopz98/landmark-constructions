c8bee16de5953d5ecbe788555a28b96b184bd373
rafipopz98 <itsrealrafi@gmail.com>
2023-10-14 23:11:11 +0530

    facebook pixel

--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,19 @@ import HomePage from "../components/Home/HomePage";
 import Residencies from "../components/Residence/Residence";
 import Value from "../components/Value/Value";
 import { useEffect } from "react";
+import HomeHome from "../../_pages/_app";
 
 export default function Home() {
   useEffect(() => {
     AOS.init();
   }, []);
   return (
-    <main>
-      <HomePage />
-      <Service />
-      <Residencies />
-      <Value />
-    </main>
+    // <main>
+    //   <HomePage />
+    //   <Service />
+    //   <Residencies />
+    //   <Value />
+    // </main>
+    <HomeHome />
   );
 }
